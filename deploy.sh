6710d65810423f9a5b3597cee8d098a9f2a8d022
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2019-12-28 19:23:12 -0800

    Stop images before building

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 
 #!/usr/bin/env bash
 git pull origin master
-docker build -t greenback-ui .
 docker stop greenback-ui
+docker rm greenback-ui
+docker build -t greenback-ui .
 docker run -p 5000:5000 -d greenback-ui
